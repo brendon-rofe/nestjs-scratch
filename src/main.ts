c8eb7a7dad46a01c17f4482c102cf04e0ab106f3
Bravo1B9 <brofe93@gmail.com>
2023-05-25 05:27:15 +0000

    Create module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 export class AppController {
 
   @Get()
   getRootRoute() {
-    return "Hello World!";
+    return { message: "Hello World!" };
   };
 
 };
+
+@Module({
+  controllers: [AppController]
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+};
+
+bootstrap();
